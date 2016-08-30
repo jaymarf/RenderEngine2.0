b71c5935e08d36b84ad9b5d9643b41c9a0e88885
jaymar freeman <jaymarf@microsoft.com>
2016-08-29 23:03:04 -0700

    added shader loader

--- RenderEngine2/RenderEngine2/main.cpp
+++ RenderEngine2/RenderEngine2/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include<GL\glew.h>
 #include "Display.h"
-
+#include "Shader.h"
 int main(int argc, char** argv)
 {
     Display display(800, 600, "Hello world");
+    Shader shader("./res/basicShader");
+
     while (display.IsClosed())
     {
-        glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
+        display.Clear(0.0f, 0.0f, 1.0f, 0.0f);
+        shader.Bind();
         display.SwapBuffers();
     }
     return 0;
